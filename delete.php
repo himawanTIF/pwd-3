072628f4eca297116d6b13569455ef49d0cd6633
himawanTIF <40656518+himawanTIF@users.noreply.github.com>
2020-10-22 17:14:31 +0700

    CRUD 2
    - Perbaikan file koneksi
    - Penyelesaian file delete

--- delete.php
+++ delete.php
@@ -5,5 +5,9 @@ include_once("koneksi.php");
 // Mendapatkan id daru URL untuk hapus data
 $nim = $_GET['nim'];
 
+// Hapus baris data dari tabel mahasiswa yang diberi id
+$result = mysqli_query($con, "DELETE FROM mahasiswa WHERE nim='$nim'");
 
+// Setelah berhasil menghapus, mengalihkan ke homepage untuk menampilkan daftar data mahasiswa
+header("Location:index.php");
 ?>
--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
-$host="localhost";
-$username="root";
-$password="";
-$databasename="akademik";
-$con=mysqli_connect($host,$username,$password,$databasename);
+$host = "localhost";
+$username = "root";
+$password = "";
+$databasename = "akademik";
+$con = mysqli_connect($host,$username,$password,$databasename);
 
 if(!$con){
     echo "Error: ".mysqli_connect_error();
